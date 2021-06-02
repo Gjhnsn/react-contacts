db510c7e4ceed00cff70234fcb032c49390b6aef
Garrett <garrettjtrains@gmail.com>
2021-06-02 15:44:26 -0700

    styled componenets, ContactCard and ContactList

--- src/components/contacts/ContactCard.js
+++ src/components/contacts/ContactCard.js
@@ -1,20 +1,79 @@
-const ContactCard = ({ firstName='', lastName='', phoneNumber='', profilePic='' }) => {
-    return (
+import styled from "styled-components";
+
+const ContactCard = ({
+  firstName = "",
+  lastName = "",
+  phoneNumber = "",
+  profilePic = "",
+}) => {
+  return (
+    <Card>
+      <InfoWrapper>
         <div>
-            <div>
-                <div>
-                    <img src={profilePic} alt={`Profile of ${firstName} ${lastName}.`} />
-                </div>
-                <div>
-                    <p>{firstName} {lastName}</p>
-                    <p>{phoneNumber}</p>
-                </div>
-            </div>
-            <div>
-                <button>X</button>
-            </div>
+          <Avatar
+            src={profilePic}
+            alt={`Profile of ${firstName} ${lastName}.`}
+          />
         </div>
-    )
-}
+        <ContactInfo>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <p>{phoneNumber}</p>
+        </ContactInfo>
+      </InfoWrapper>
+      <ButtonWrapper>
+        <Button>X</Button>
+      </ButtonWrapper>
+    </Card>
+  );
+};
+
+const Card = styled.div`
+  height: 120px;
+  width: 600px;
+  box-shadow: 0px 3px 8px grey;
+  display: flex;
+  justify-content: space-between;
+  margin: 20px;
+  background-color: #fffffd;
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Avatar = styled.img`
+  border-radius: 50%;
+  height: 100px;
+  width: auto;
+  position: relative;
+  margin-left: 15px;
+`;
+
+const ContactInfo = styled.div`
+  margin-left: 20px;
+  padding-left: 20px;
+  border-left: 1px solid lightgrey;
+`;
+
+const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
+const Button = styled.button`
+  border-radius: 50%;
+  border: none;
+  height: 1.5rem;
+  width: 1.5rem;
+  font-size: 1rem;
+  color: white;
+  font-weight: 800;
+  background-color: #999;
+`;
 
 export default ContactCard;
--- src/components/contacts/ContactsList.js
+++ src/components/contacts/ContactsList.js
@@ -1,19 +1,30 @@
-import ContactCard from "./ContactCard"
+import ContactCard from "./ContactCard";
+import styled from "styled-components";
 
-const ContactList = ({ contacts=[] }) => {
-    return (
-        <div>
-            {contacts.map((contact) => 
-                <ContactCard 
-                    key={contact.id}
-                    firstName={contact.firstName} 
-                    lastName={contact.lastName} 
-                    phoneNumber={contact.phoneNumber} 
-                    profilePic={contact.profilePic}
-                />
-            )}
-        </div>
-    )
-}
+const ContactList = ({ contacts = [] }) => {
+  return (
+    <ContactsList>
+      {contacts.map((contact) => (
+        <ContactCard
+          key={contact.id}
+          firstName={contact.firstName}
+          lastName={contact.lastName}
+          phoneNumber={contact.phoneNumber}
+          profilePic={contact.profilePic}
+        />
+      ))}
+    </ContactsList>
+  );
+};
+
+const ContactsList = styled.div`
+  background-color: #ddd;
+  width: 800px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
+  border: 1px solid grey;
+`;
 
 export default ContactList;
